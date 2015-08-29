d48d1f43ac706c2986def36f169235679d1cd8d1
Hyunsik, Kim <wbstory@kisti.re.kr>
2015-08-29 17:18:34 +0900

    Suggstion demo:completed.

--- js/suggestion.js
+++ js/suggestion.js
@@ -7,11 +7,11 @@ var Suggestion = (function(module) {
       return x - Math.floor(x);
   }
   
-  module.setSeed = function(x) {
+  var setSeed = function(x) {
     seed = x;
   }
   
-  module.makeMatrix = function() {
+  var makeMatrix = function() {
     var seed = $('#random_seed').val() - 0;
     var rated_movies = $('#rated_movies').val() - 0;
     var unrated_movies = $('#unrated_movies').val() - 0;
@@ -26,7 +26,7 @@ var Suggestion = (function(module) {
     return m1.valueOf();
   }
 
-  module.makeRatingTable = function(target, data) {
+  var makeRatingTable = function(target, data) {
     var user_count = $('#user_count').val() - 0;
     var rated_movies = $('#rated_movies').val() - 0;
 
@@ -64,7 +64,7 @@ var Suggestion = (function(module) {
     target.append(head).append(body);
   }
 
-  module.makeSimilarityTable = function(target, data) {
+  var makeSimilarityTable = function(target, data) {
     var rated_movies = $('#rated_movies').val() - 0;
     var unrated_movies = $('#unrated_movies').val() - 0;
 
@@ -76,7 +76,7 @@ var Suggestion = (function(module) {
     header_row.append("<th>Movie</th>");
 
     for(var i=0; i<unrated_movies; i++) {
-      header_row.append("<th>M"+(i+6)+"</th>");
+      header_row.append("<th>M"+(i+rated_movies)+"</th>");
     }
 
     head.append(header_row);
@@ -97,7 +97,7 @@ var Suggestion = (function(module) {
     target.append(head).append(body);
   }
 
-  module.extractMatrix = function(target) {
+  var extractMatrix = function(target) {
     var data = [];
 
     $(target).find("tbody")
@@ -106,15 +106,63 @@ var Suggestion = (function(module) {
         $(d).find("input").each(function(i, d) {
           row.push($(d).val()-0);
         });
-        console.log(row);
         data.push(row);
       });
 
     return data;
   }
 
-  module.restoreMatrix = function(target, data) {
+  var makeSuggestionMatrix = function(user_count, ratings, sim_table, suggestion_count) {
+    var user_friendly = math.zeros(user_count, sim_table[0].length).valueOf();
 
+    for(var userIdx=0; userIdx<user_count; userIdx++) {
+      for(var ratedMovieIdx=0; ratedMovieIdx<ratings[userIdx].length; ratedMovieIdx++) {
+        var rating = ratings[userIdx][ratedMovieIdx];
+        var scores = sim_table[ratedMovieIdx].map(function(d) { return d * rating; });
+
+        user_friendly[userIdx] = _.zipWith(user_friendly[userIdx], scores, _.add);
+      }
+    }
+
+    var users = [];
+    for(var userIdx=0; userIdx<user_count; userIdx++) {
+      var highestScore = _(user_friendly[userIdx]).sortBy().takeRight(suggestion_count).reverse().value();
+      var movies = highestScore.map(function(score) {
+        return [_(user_friendly[userIdx]).indexOf(score), score];
+      });
+
+      users.push(movies);
+    }
+
+    return users;
+  }
+
+  //////////////////////////////////
+  // Event listeners
+  //////////////////////////////////
+  module.suggestion = function() {
+    var target = $("#movie_suggestion_table tbody");
+    var user_count = $('#user_count').val() - 0;
+    var ratings = extractMatrix($("#movie_rating_table"));
+    var sim_table = extractMatrix($("#movie_similarity_table"));
+    var suggestion_count = $("#suggestion_count").val() - 0;
+
+    var m2 = makeSuggestionMatrix(user_count, ratings, sim_table, suggestion_count);
+
+    var rated_movies = $('#rated_movies').val() - 0;
+
+    target.empty();
+
+    m2.map(function(user, userIdx) {
+      var row = $("<tr>");
+      row.append($("<td>U"+(userIdx+1)+"</td>"))
+      var col = $("<td>");
+      user.map(function(movies) {
+        col.append("<span>M"+(movies[0]+rated_movies)+"("+parseInt(movies[1]*1000)+") </span>");
+      });
+      row.append(col);
+      target.append(row);
+    });
   }
 
   module.update = function() {
